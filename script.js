74e8707f9b4eec4420072e043b71fdf16cd7d109
Diego <ziraldodiego@gmail.com>
2019-07-15 17:42:07 -0300

    2

---
+++ script.js
@@ -0,0 +1,27 @@
+
+
+window.onclick = function(){
+    
+    var boton1 = document.getElementById('boton1').className='nav-link';
+    var boton2 =  document.getElementById('boton2').className='nav-link active';
+}
+
+
+
+
+
+
+/* AOS.init({delay: 1000, duration: 1000});
+
+window.onscroll = function() {
+    if (window.pageYOffset > 10) {
+        // Agregar clase nav
+        // .menu-scroll
+        //document.getElementsByTagName('nav')[0].className = '';
+        document.getElementById('menu').className = 'row fixed-top menu-scroll';
+    } else {
+        // Sacar clase del nav
+        document.getElementById('menu').className = 'row fixed-top';
+    }
+    //console.log(window.pageYOffset);
+} */
